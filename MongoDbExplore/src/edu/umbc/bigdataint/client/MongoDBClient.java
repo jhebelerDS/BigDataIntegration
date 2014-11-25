ec7d310e24be823f54579dadf34d0d649b4ba584
John Hebeler <jhebeler@gmail.com>
2014-11-25 03:00:17 +0000

    gh

---
+++ MongoDbExplore/src/edu/umbc/bigdataint/client/MongoDBClient.java
@@ -0,0 +1,26 @@
+package edu.umbc.bigdataint.client;
+import com.mongodb.BasicDBObject;
+import com.mongodb.BulkWriteOperation;
+import com.mongodb.BulkWriteResult;
+import com.mongodb.Cursor;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.ParallelScanOptions;
+
+import java.util.List;
+import java.util.Set;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+
+public class MongoDBClient {
+
+	public static void main(String[] args) {
+		MongoDBClient mongoClient = new MongoDBClient();
+
+	}
+
+}
