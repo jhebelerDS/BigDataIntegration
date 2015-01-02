530f9f5bdbd06a5314a9304a3a0ff4341310e877
John Hebeler <jhebeler@gmail.com>
2015-01-02 20:15:05 +0000

    Added ability to count structures using simple compare

--- MongoDbExplore/src/edu/umbc/bigdataint/client/MongoDBClient.java
+++ MongoDbExplore/src/edu/umbc/bigdataint/client/MongoDBClient.java
@@ -11,9 +11,14 @@ import com.mongodb.MongoClient;
 import com.mongodb.ParallelScanOptions;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Set;
+
+import org.bson.BSONObject;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
@@ -37,6 +42,10 @@ public class MongoDBClient {
 		int randomNum = 0;
 		int BATCHSIZE = 5;
 		
+		ArrayList<DBObject> structures = new ArrayList<DBObject>();
+		ArrayList<DBObject> structuresTmp = new ArrayList<DBObject>();
+		
+		
 		
 		
 		for(int sampNum = 1 ; sampNum < MAXSAMPLES+1; sampNum++){
@@ -45,13 +54,65 @@ public class MongoDBClient {
 			long numRecords = coll.count();
 			System.out.println("NUMBER of RECORDS: " + numRecords);
 
-			randomNum = rand.nextInt(95);
+			randomNum = rand.nextInt(1995);
 			cursor.skip(randomNum);
 			
-			DBObject obj = cursor.next();
-			System.out.println("SAMPLE " + sampNum + ": " + obj);
+			// You have your starting point - now print out five
+			for(int j = 0; j<BATCHSIZE ; j++){
+  			   DBObject obj = cursor.next();
+			   System.out.println("SAMPLE " + sampNum + ": " + obj);
+			   // Step through DB objects
+			   if (structures.size() == 0) {
+			   	   // Must seed the array
+				   structures.add(obj);
+			   } else {
+				   // Figure out if this is the same as ANY of the saved structures
+				   // Set flag to see if the same
+				   boolean same = false;
+				   for(DBObject saveObj: structures){
+					   if( sameDBObjects(saveObj, obj) ){
+						   same = true;
+						   break;
+					   }
+				   }
+				   if(!same){
+					   structures.add(obj);
+				   }
+			   }
+			
+			}
 		}
 		
+		System.out.println("Number of Structures Found: " + structures.size());
+		
+	}
+
+	private boolean sameDBObjects(DBObject saveObj, DBObject obj) {
+		boolean different = true;
+		
+		Map saveObjMap = saveObj.toMap();
+		Set saveObjSet = saveObjMap.keySet();
+		Iterator saveObjIt = saveObjSet.iterator();
+		
+		Map objMap = obj.toMap();
+		Set objSet = objMap.keySet();
+		if( objSet.isEmpty() ){
+			return true;
+		}
+		Iterator objIt = objSet.iterator();
+		
+		while(saveObjIt.hasNext()){
+			String compareKey = saveObjIt.next().toString();
+			System.out.println("Comparing: " + compareKey );
+			if(!obj.containsField(compareKey)){
+				different = false;
+				System.out.println("KEY NOT FOUND");
+				break;
+			}
+		}
+	    
+		return different;
+
 	}
 
 	public MongoDBClient () throws UnknownHostException{
@@ -75,6 +136,16 @@ public class MongoDBClient {
 		   coll.insert(doc);
 		}
 		
+		for(int i = 0; i <1000 ; i++ ){
+			
+			   BasicDBObject doc = new BasicDBObject("place", "fisbine" + i)
+			     .append("city", "trenton")
+			     .append("population", i+1000)
+			     .append("additional", new BasicDBObject("x1", 203+i).append("y1", 102+ i));
+			   coll.insert(doc);
+			}
+		
+		System.out.println("TOTAL RECORDS: " + coll.getCount());
 		DBObject myDoc = coll.findOne();
 		
 		//System.out.println(myDoc);
