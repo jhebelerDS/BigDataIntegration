ff4cfefb967e8601944fb039981e65374febb658
jhebeler <jhebeler@gmail.com>
2015-03-29 16:09:42 -0400

    Correction of Pom

--- BigDataStructureProbe/src/main/java/umbc/hebeler/phd/probe/ProbeAccumulo.java
+++ BigDataStructureProbe/src/main/java/umbc/hebeler/phd/probe/ProbeAccumulo.java
@@ -1,6 +1,15 @@
 package umbc.hebeler.phd.probe;
 
 
+import org.apache.accumulo.core.client.AccumuloException;
+import org.apache.accumulo.core.client.AccumuloSecurityException;
+import org.apache.accumulo.core.client.Connector;
+import org.apache.accumulo.core.client.Instance;
+import org.apache.accumulo.core.client.ZooKeeperInstance;
+import org.apache.accumulo.core.client.security.tokens.AuthenticationToken;
+import org.apache.accumulo.core.client.security.tokens.PasswordToken;
+import org.apache.accumulo.core.conf.AccumuloConfiguration;
+
 import com.hp.hpl.jena.rdf.model.Model;
 
 public class ProbeAccumulo implements Probe {
@@ -10,7 +19,15 @@ public class ProbeAccumulo implements Probe {
 		 Instance instances = new ZooKeeperInstance("accumulo", "accumuloServer");
 		 AccumuloConfiguration ac = instances.getConfiguration();
 		 AuthenticationToken token = new PasswordToken("123456789");
-		 Connector conn = instances.getConnector("root",token);
+		 try {
+			Connector conn = instances.getConnector("root",token);
+		} catch (AccumuloException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (AccumuloSecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		 
 		return false;
 	}
